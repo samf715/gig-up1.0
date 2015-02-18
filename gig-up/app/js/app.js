cd0811adbf2bc9012b339c81f6b52cfe20b7cc4e
diennguyen90 <diennguyen90@yahoo.com>
2015-02-18 01:48:54 -0800

    worked on the homepage a bit with working partials

--- gig-up/app/js/app.js
+++ gig-up/app/js/app.js
@@ -22,6 +22,16 @@ gigupApp.config(['$routeProvider',
         templateUrl: 'partials/event-detail.html',
         controller: 'EventDetailCtrl'
       }).
+        //created a loginhtml in partials temporary - dien
+        when('/login',{
+            templateUrl: 'partials/login.html',
+            controller: 'LoginController'
+        }).
+        //will work on later to give function and form
+        when('/addEvent',{
+            templateUrl: 'partials/addEvent.html',
+            controller: 'AddEventController'
+        }).
       otherwise({
         redirectTo: '/events'
       });
